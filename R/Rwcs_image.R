868a318e4078db1a2279b0a50cb7ed25d3bd14fb
Aaron Robotham <aaron.robotham@uwa.edu.au>
2021-06-14 15:32:37 +0800

    Direction option.

--- R/Rwcs_image.R
+++ R/Rwcs_image.R
@@ -2,7 +2,7 @@ Rwcs_image=function(image, keyvalues=NULL, n, grid.col='grey', grid.lty=2, grid.
                     lab.col='green', coord.type='sex', margin=TRUE, loc.diff=c(0,0),
                     xlab='Right Ascension', ylab='Declination', mgp=c(2,0.5,0), mtline=2,
                     position='topright', com.col="green", com.length=0.05,
-                    coord.axis='auto', pretty='auto', header=NULL, add=FALSE, ...){
+                    coord.axis='auto', pretty='auto', header=NULL, add=FALSE,  direction='auto', ...){
   
   if(missing(xlab)){
     if(coord.type=='sex'){
