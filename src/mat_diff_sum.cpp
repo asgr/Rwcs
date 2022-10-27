add71fe68fb40971b9b07c03c657d404bdf0b4ab
Aaron Robotham <aaron.robotham@uwa.edu.au>
2022-10-27 09:30:00 +0800

    Better strictness (Caro Foster)

--- src/mat_diff_sum.cpp
+++ src/mat_diff_sum.cpp
@@ -9,9 +9,9 @@ double mat_diff_sum(Rcpp::NumericMatrix mat1, Rcpp::NumericMatrix mat2, double s
   Rcpp::NumericMatrix m(nrow,ncol);
   for (int i = 0; i < nrow; i++) {
     for (int j = 0; j < ncol; j++) {
-      if(i - delta_x >= 0 & i - delta_x < nrow){
-        if(j - delta_y >= 0 & j - delta_y < ncol){
-          if(isfinite(mat1(i,j)) & isfinite(mat2(i - delta_x, j - delta_y))){
+      if(i - delta_x >= 0 && i - delta_x < nrow){
+        if(j - delta_y >= 0 && j - delta_y < ncol){
+          if(std::isfinite(mat1(i,j)) && std::isfinite(mat2(i - delta_x, j - delta_y))){
             mat_sum += pow((mat1(i, j) - mat2(i - delta_x, j - delta_y))/scale,2);
           }
         }
